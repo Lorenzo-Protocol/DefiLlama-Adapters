6a4cb511f65705ce364040bdb2f914003a405640
WangWilly <s89667654@gmail.com>
2025-10-10 14:09:25 +0800

    refactor: 💡 apply mapping

--- projects/lorenzo-susd1plus/index.js
+++ projects/lorenzo-susd1plus/index.js
@@ -34,9 +34,15 @@ const config = {
   },
 };
 
+// susd1p to usd1
+const TOKEN_MAPPINGS = {
+  '0x1c290af93a0fD565E92E5e7d9045F35b1e9ef71d': '0x8d0D000Ee44948FC98c9B98A4FA4921476f08B0d',
+  '0x4F2760B32720F013E900DC92F65480137391199b': '0x8d0D000Ee44948FC98c9B98A4FA4921476f08B0d',
+}
+
 /**
   * @param {import('@defillama/sdk').ChainApi} api - DefiLlama Chain API instance
-  * @returns {Promise<void>}
+  * @returns {Promise<import('@defillama/sdk').BalancesV1>} - The balances object containing token balances
   */
 async function tvl(api) {
   const chain = api.chain;
@@ -46,8 +52,11 @@ async function tvl(api) {
   const tvlValue = data?.tvlByChain?.tvl;
 
   if (tvlValue) {
-    api.add(contractAddr, tvlValue);
+    const targetToken = TOKEN_MAPPINGS[contractAddr] || contractAddr;
+    api.add(targetToken, tvlValue);
   }
+
+  return api.getBalances();
 }
 
 module.exports = {
